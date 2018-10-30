632fc1c4cb0b45f68e66858a401bcb97256c0add
Zachary Lehmann <zlehmann@gmail.com>
2018-10-30 21:51:45 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,7 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT title FROM projects LEFT OUTER JOIN pledges ON project.id = pledges.id;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
