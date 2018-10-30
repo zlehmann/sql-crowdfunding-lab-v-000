a5f423704d3fc301601d0a1abce7967d29a8e132
Zachary Lehmann <zlehmann@gmail.com>
2018-10-30 21:41:56 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,34 +1,34 @@
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ();
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ();
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ();
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ();
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ();
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ();
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ();
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ();
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ();
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ();
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01'),
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30'),
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Animal shelter needs dog food', 'charity', 400.00, '2013-03-14', '2013-06-30'),
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Voldement needs a body', 'charity', 6000.00, '2013-03-20', '2013-09-20'),
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('The next Inna-Gadda-Davida', 'music', 200.00, '2014-12-30', '2015-12-30'),
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Iguana needs tail operation', 'charity', 2000.00, '2013-10-02', '2013-10-30'),
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31'),
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30'),
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30')
 
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
+INSERT INTO users (name, age) VALUES ("Vultirnus Telvek", 74);
+INSERT INTO users (name, age) VALUES ("Vultirnus Telvek", 74);
+INSERT INTO users (name, age) VALUES ("Vultirnus Telvek", 74);
+INSERT INTO users (name, age) VALUES ("Vultirnus Telvek", 74);
+INSERT INTO users (name, age) VALUES ("Vultirnus Telvek", 74);
+INSERT INTO users (name, age) VALUES ("Vultirnus Telvek", 74);
+INSERT INTO users (name, age) VALUES ("Vultirnus Telvek", 74);
+INSERT INTO users (name, age) VALUES ("Vultirnus Telvek", 74);
+INSERT INTO users (name, age) VALUES ("Vultirnus Telvek", 74);
+INSERT INTO users (name, age) VALUES ("Vultirnus Telvek", 74);
+INSERT INTO users (name, age) VALUES ("Vultirnus Telvek", 74);
+INSERT INTO users (name, age) VALUES ("Vultirnus Telvek", 74);
+INSERT INTO users (name, age) VALUES ("Vultirnus Telvek", 74);
+INSERT INTO users (name, age) VALUES ("Vultirnus Telvek", 74);
+INSERT INTO users (name, age) VALUES ("Vultirnus Telvek", 74);
+INSERT INTO users (name, age) VALUES ("Vultirnus Telvek", 74);
+INSERT INTO users (name, age) VALUES ("Vultirnus Telvek", 74);
+INSERT INTO users (name, age) VALUES ("Vultirnus Telvek", 74);
+INSERT INTO users (name, age) VALUES ("Vultirnus Telvek", 74);
+INSERT INTO users (name, age) VALUES ("Vultirnus Telvek", 74);
 
 INSERT INTO pledges (amount, user_id, project_id) VALUES ();
 INSERT INTO pledges (amount, user_id, project_id) VALUES ();
