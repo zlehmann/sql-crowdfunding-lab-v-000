fb2cbddb834386b9614c6575b3ccc38054de6e6e
Zachary Lehmann <zlehmann@gmail.com>
2018-10-30 23:54:32 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, SUM(pledges.amount) - projects.funding_goal FROM projects INNER JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title;"
+"SELECT projects.title, SUM(pledges.amount) - projects.funding_goal FROM projects INNER JOIN pledges ON projects.id = pledges.project_id WHERE SUM(pledges.amount) - projects.funding_goal > 0 GROUP BY projects.title;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
