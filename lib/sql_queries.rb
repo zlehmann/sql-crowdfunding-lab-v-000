bdf6dfc01ace99b85a8e275bd026aaad376d03c2
Zachary Lehmann <zlehmann@gmail.com>
2018-10-31 00:07:17 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, SUM(pledges.amount) FROM users INNER JOIN pledges on users.id = pledges.user_id GROUP BY users.name ORDER BY SUM(pledges.amount)"
+"SELECT users.name, pledges.amount FROM users INNER JOIN pledges on users.id = pledges.user_id GROUP BY users.name ORDER BY SUM(pledges.amount)"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
