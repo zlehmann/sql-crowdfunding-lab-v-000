cfdd77c6882f66ceab2e6b1aca93a34fea7a7b00
Zachary Lehmann <zlehmann@gmail.com>
2018-10-31 00:10:26 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,33 @@
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT
+);
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER
+);
+
+CREATE TABLE projects_pledges (
+  id INTEGER PRIMARY KEY,
+  project_id INTEGER,
+  pledge_id INTEGER
+);
+
+CREATE TABLE projects_users (
+  id INTEGER PRIMARY KEY,
+  project_id INTEGER,
+  user_id INTEGER
+);
